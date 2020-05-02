b821e0ef49c8a2123ab67e17f1bbd5c687059049
sangeet das <imsangeet3@gmail.com>
2020-05-02 14:55:24 +0530

    iris modified

--- project/Iris-data-prediction.py
+++ project/Iris-data-prediction.py
@@ -18,38 +18,40 @@ df = pd.read_csv('E:\\pythonDatasets\\irisData\\iris-data.csv', header=None)
 # print (df.head())
 
 X = df.iloc[:, :-1]
+X = np.array(X).astype(np.float)
+
 y = df.iloc[:, -1]
 print ('typw of X from dataframe : ', type(X))
-print (X.iloc[0], ' -- ', y[0])
+print (X, ' -- ', y[0])
 
 outputs_vals, outputs_ints = np.unique(y, return_inverse=True)
 # print (outputs_ints)
 print (outputs_vals)
 
+y_cat = to_categorical(outputs_ints)
+print(y_cat)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y_cat, test_size=0.2, random_state=42)
+
+print(X_train.shape)
+print(y_train.shape)
+
+scalar = MinMaxScaler()
+X_train = scalar.fit_transform(X_train)
+X_test = scalar.transform(X_test)
+
 # plt.plot(X_train)
 # plt.show()
 def doModeling():
 
-    y_cat = to_categorical(outputs_ints)
-    print(y_cat)
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y_cat, test_size=0.2, random_state=42)
-
-    print(X_train.shape)
-    print(y_train.shape)
-
-    scalar = MinMaxScaler()
-    X_train = scalar.fit_transform(X_train)
-    X_test = scalar.transform(X_test)
-
     model = Sequential()
-    model.add(Dense(32, activation='sigmoid', input_shape=(X_train.shape[1],)))
+    model.add(Dense(25, activation='sigmoid', input_shape=(X_train.shape[1],)))
     # model.add(Dropout(0.23))
     model.add(Dense(3, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
 
-    model_history = model.fit(X_train, y_train, epochs=100,
+    model_history = model.fit(X_train, y_train, epochs=150,
               validation_data=(X_test, y_test))
 
     metric = model.evaluate(X_test, y_test)
@@ -78,19 +80,20 @@ def doModeling():
 
 def doPrediction():
     model = load_model('iris-data-model.h5')
-    pred = model.predict(visualizeTestData())
+    xtest, ytest = visualizeTestData()
+    pred = model.predict(xtest)
     print ('pred : ', pred)
-
 
 def visualizeTestData():
     test = df.to_numpy()
     # print (test[0])
-    xtest = test[11][:-1]
-    ytest = test[11][-1:]
-    print (xtest.reshape(1,4))
-    print (ytest)
-    return np.asarray(xtest.reshape(1,4), dtype='float32')
+    xtest = test[132][:-1]
+    xtest = np.asarray(xtest.reshape(1, 4), dtype='float32')
+    xtest = scalar.transform(xtest)
+    ytest = test[132][-1:]
+    return xtest, ytest
 
 # doModeling()
-# visualizeTestData()
+xtest, ytest = visualizeTestData()
+print (xtest, ' is ', ytest)
 doPrediction()
